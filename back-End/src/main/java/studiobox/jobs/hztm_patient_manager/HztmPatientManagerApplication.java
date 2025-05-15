9bd6712564c34ac8a6d231c54db9dca5997e748b
Dawud Albouz <dawud.albouz96@gmail.com>
2025-05-15 17:09:29 +0200

    add back-end

---
+++ back-End/src/main/java/studiobox/jobs/hztm_patient_manager/HztmPatientManagerApplication.java
@@ -0,0 +1,13 @@
+package studiobox.jobs.hztm_patient_manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HztmPatientManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HztmPatientManagerApplication.class, args);
+	}
+
+}
---
+++ back-End/src/test/java/studiobox/jobs/hztm_patient_manager/HztmPatientManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package studiobox.jobs.hztm_patient_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HztmPatientManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
